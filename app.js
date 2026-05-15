ca2ddb2cd39791c1be7c38ff74c9e5447a88ec19
gpt0507 <gauravagrawal7002@gmail.com>
2026-05-15 23:11:15 +0530

    ordered mail added with process env

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: "",
-    pass: ""
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
   }
 });
 
