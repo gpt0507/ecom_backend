42b760267b313cd1521b73f131f88574feab787c
gpt0507 <gauravagrawal7002@gmail.com>
2026-05-23 12:35:22 +0530

    finally completed

--- app.js
+++ app.js
@@ -1,63 +1,63 @@
-  require('dotenv').config();
-  const express = require("express");
-  const app = express();
-  const Razorpay = require("razorpay");
-  const path = require("path");
-  const crypto = require("crypto");
-  const nodemailer = require("nodemailer");
-  const ejs = require("ejs");
-  const cors = require('cors');
-  const port = 4009
-  require('./config/db')
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const Razorpay = require("razorpay");
+const path = require("path");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const cors = require('cors');
+const port = 4009
+require('./config/db')
 
-  app.use(cors());
-  app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-  app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
-  // view engine
-  app.set("view engine", "ejs");
+// view engine
+app.set("view engine", "ejs");
 
-  // static folder
-  app.use(express.static("public"));
-
-
-  // Nodemailer Transporter
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD
-    }
-  });
+// static folder
+app.use(express.static("public"));
 
 
-  // user route
-  const userRoutes = require("./routes/userRoutes");
-  app.use("/api/users", userRoutes);
-
-  // products route
-  const productRoutes = require("./routes/productRoutes");
-  app.use("/api/products", productRoutes);
-
-
-  // admin route
-  const adminRoutes = require("./routes/adminRoutes");
-  app.use("/api/admin", adminRoutes);
-
-  // admin route
-  const paymentRoutes = require("./routes/paymentRoutes");
-  app.use("/api/payment", paymentRoutes);
-
-  // order route
-  const orderRoutes = require("./routes/orderRoutes");
-  app.use("/api/orders", orderRoutes);
+// Nodemailer Transporter
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
 
 
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
+// user route
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
 
-  module.exports = app;
+// products route
+const productRoutes = require("./routes/productRoutes");
+app.use("/api/products", productRoutes);
+
+
+// admin route
+const adminRoutes = require("./routes/adminRoutes");
+app.use("/api/admin", adminRoutes);
+
+// admin route
+const paymentRoutes = require("./routes/paymentRoutes");
+app.use("/api/payment", paymentRoutes);
+
+// // order route
+// const orderRoutes = require("./routes/orderRoutes");
+// app.use("/api/orders", orderRoutes);
+
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+module.exports = app;
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createOrder, verifyPayment, } = require("../controllers/paymentController");
+const { createOrder, verifyPayment, sendOrderMail } = require("../controllers/paymentController");
 
 // CREATE ORDER
 router.post("/create-order", createOrder);
@@ -8,5 +8,7 @@ router.post("/create-order", createOrder);
 // VERIFY PAYMENT
 router.post("/verify-payment", verifyPayment);
 
+// SEND MAIL
+router.post("/send-order-mail", sendOrderMail);
 
 module.exports = router;
